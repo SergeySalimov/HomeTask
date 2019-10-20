ccdb7b458f6915c9d3c00bf6004733cce26f438a
Salimov Sergey <salimov_s@tut.by>
2019-10-20 21:10:19 +0300

    feat (hm-by-js): done

--- home-work-13/script.js
+++ home-work-13/script.js
@@ -2,15 +2,6 @@ let num = prompt('Input any positiv whole number:');
 let numLenght = num.length;
 num = +num;
 console.log('Your number by digit is:');
-// variant 1 рабочий
-// for (let i = numLenght; i > 0; i--) {
-//   let digitNumber = Math.floor(num / (10 ** (i - 1)));
-//   console.log(digitNumber);
-//   num -= digitNumber * (10 ** (i - 1));
-// }
-
-// variant 2 без дополнительной переменной
-
 for (let i = numLenght; i > 0; i--) {
   let delitelRazryda = 10**(i-1);
   console.log(Math.floor(num / delitelRazryda));
