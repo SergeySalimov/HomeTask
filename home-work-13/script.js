e4fb5e5712b61492d68ea7aa6c475b6428c2618e
SergeySalimov <salimov_s@tut.by>
2019-10-23 16:47:23 +0300

    feat(home-work-13 || js - 1 ): add function getDigitLenght

--- home-work-13/script.js
+++ home-work-13/script.js
@@ -1,9 +1,5 @@
 let num = +prompt('Input any positiv whole number:');
-
-// let numLenght = num.length;
 let numLenght = getDigitLenght(num);
-
-// num = +num;
 console.log('Your number by digit is:');
 for (let i = numLenght; i > 0; i--) {
   let delitelRazryda = 10**(i-1);
